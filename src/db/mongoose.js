f0f35358ab22cb336a0f4281d9de68817003765e
Marlon Lopez <marlonlop.cs@gmail.com>
2020-11-12 21:58:50 -0700

    basic user model added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,1 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.01:27017/task-manager-api', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
