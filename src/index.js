cf9d73b07525b86d0451ceec46d9596c24863ce6
Marlon Lopez <marlonlop.cs@gmail.com>
2020-11-14 16:53:06 -0700

    get users and get user enpoints added

--- src/index.js
+++ src/index.js
@@ -18,6 +18,24 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.get('/users', (req, res) => {
+    User.find().then((users) => {
+        res.send(users);
+    }).catch((e) => {
+        res.status(500).send(e);
+    });
+});
+
+app.get('/user/:id', (req, res) => {
+    // console.log(req.params.id) // id parameter returned
+    User.findById(req.params.id).then((user) => {
+        if(!user) {return res.status(404).send();}
+        res.send(user);
+    }).catch((e) => {
+        res.status(500).send(e);
+    });
+});
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
 
